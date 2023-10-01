a801b5285c23b212712203af1dc837df9697cc24
EgorTovstik <eg1707or2003@gmail.com>
2023-10-02 00:12:27 +0700

    Y

--- keyboard.py
+++ keyboard.py
@@ -10,7 +10,7 @@ main_kb = [
 main = ReplyKeyboardMarkup(keyboard=main_kb,
                            resize_keyboard=True)
 
-report1 = InlineKeyboardMarkup(inline_keyboard=[
+report = InlineKeyboardMarkup(inline_keyboard=[
    [InlineKeyboardButton(text='Количество оценок', callback_data = 'quantity')],
    [InlineKeyboardButton(text='Личные номера студентов', callback_data = 'number')],
    [InlineKeyboardButton(text='Формы контроля', callback_data = 'forms')]
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 import os
 import asyncio
+import logging
+import sys
 import pandas as pd
 import keyboard as kb
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, CallbackQuery
+from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, F, Router, html
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 from aiogram.filters import CommandStart
 
    
@@ -13,20 +18,41 @@ load_dotenv()
 data = pd.read_excel("lab_pi_101.xlsx")
 
 
-bot = Bot(token = os.getenv("TOKEN"))
+TOKEN = os.getenv("TOKEN")
 dp = Dispatcher()
 
+form_router = Router()
 
 
-@dp.message(CommandStart()) 
-async def send_welcome(message: Message):
+class Form(StatesGroup):
+   name = State()
+   
+
+@form_router.message(CommandStart())
+async def command_start(message: Message):
    await message.answer("Добро пожаловать!")
    await message.answer("Задача этого бота заключается в отображении запроса из excel базы данных.", reply_markup=kb.main)
 
+   
+   
+@form_router.message(F.text == 'Получить отчет')
+async def report(message: Message, state: FSMContext) -> None:
+   await state.set_state(Form.name)
+   await message.answer(
+        "Введите номер группы: ",
+        reply_markup=ReplyKeyboardRemove())
 
-@dp.message(F.text == 'Получить отчет')
-async def report(message: Message):
-   await message.answer('Выберите отчет какого типа Вы хотите получить:', reply_markup=kb.report1)
+   
+@form_router.message(Form.name)
+async def process_name(message: Message, state: FSMContext) -> None:
+   await state.update_data(name=message.text)
+   await message.answer(f"Номер вашей группы:  {html.quote(message.text)}")
+   skore = data['Группа'].str.contains(str(Form.name)).sum()
+   if skore == 0:
+      await message.answer('К сожалению группы с таким номером не существует.', reply_markup=kb.main)
+   else:
+      await message.answer('Выберите отчет какого типа Вы хотите получить:', reply_markup=kb.report)
+   
 
 @dp.callback_query(F.data == 'quantity')
 async def cbquantity(callback: CallbackQuery):
@@ -58,7 +84,13 @@ async def cbquantity(callback: CallbackQuery):
 
 
 async def main():
-   await dp.start_polling(bot)
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
 
-if __name__ == '__main__':
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
